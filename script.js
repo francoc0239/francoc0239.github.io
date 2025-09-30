5d5f02b8368f64930e916eadfe7240eb1aa12f44
francoc0239 <francoc16@nycstudents.net>
2025-09-30 09:29:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Franco.C"; // update the part BETWEEN the "quotes"
+let userName = "francoc0239"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
